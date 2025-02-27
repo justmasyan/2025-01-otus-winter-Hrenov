4cb27d96eb1774958126e1cc7b2c48bad90acf2f
Maksim <maks-xrenov@mail.ru>
2025-02-27 10:51:30 +0300

    [HW-01] Финальные правки

--- hw01/src/test/java/ru/otus/hw/service/TestServiceImplTest.java
+++ hw01/src/test/java/ru/otus/hw/service/TestServiceImplTest.java
@@ -6,15 +6,14 @@ import ru.otus.hw.dao.QuestionDao;
 import ru.otus.hw.domain.Answer;
 import ru.otus.hw.domain.Question;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
-class TestServiceImplExecuteSimpleUnitTest {
+class TestServiceImplTest {
 
     private IOService ioService;
 
@@ -61,56 +60,39 @@ class TestServiceImplExecuteSimpleUnitTest {
         );
 
         List<String> exceptedOutputQuestions = List.of(
-                "Please answer the questions below",
-                "Is there life on Mars?\r\n1) Science doesn't know this yet; 2) Certainly. The red UFO is from Mars. And green is from Venus; 3) Absolutely not;",
-                "How should resources be loaded form jar in Java?\r\n1) ClassLoader#geResourceAsStream or ClassPathResource#getInputStream; 2) ClassLoader#geResource#getFile + FileReader; 3) Wingardium Leviosa;",
-                "Which option is a good way to handle the exception?\r\n1) @SneakyThrow; 2) e.printStackTrace(); 3) Rethrow with wrapping in business exception (for example, QuestionReadException); 4) Ignoring exception;",
+                "Please answer the questions below\r\n",
+                "Is there life on Mars?",
+                "1) Science doesn't know this yet; 2) Certainly. The red UFO is from Mars. And green is from Venus; 3) Absolutely not; ",
+                "How should resources be loaded form jar in Java?",
+                "1) ClassLoader#geResourceAsStream or ClassPathResource#getInputStream; 2) ClassLoader#geResource#getFile + FileReader; 3) Wingardium Leviosa; ",
+                "Which option is a good way to handle the exception?",
+                "1) @SneakyThrow; 2) e.printStackTrace(); 3) Rethrow with wrapping in business exception (for example, QuestionReadException); 4) Ignoring exception; ",
                 "What is the meaning of life?",
-                "What number did I make a wish for?\r\n1) 0; 2) 1; 3) 2; 4) 3; 5) 4; 6) 5; 7) 6; 8) 7; 9) 8; 10) 9;"
+                "What number did I make a wish for?",
+                "1) 0; 2) 1; 3) 2; 4) 3; 5) 4; 6) 5; 7) 6; 8) 7; 9) 8; 10) 9; "
         );
 
-        ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
-        PrintStream inputStream = new PrintStream(outputStreamCaptor);
+
+        List<String> actualOutputStrings = new ArrayList<>();
 
         doAnswer(invocation -> {
-            String str = invocation.getArgument(0);
-            inputStream.println(str);
+            actualOutputStrings.add(invocation.getArgument(0));
             return null;
         }).when(ioService).printLine(anyString());
 
         doAnswer(invocation -> {
             Object[] allArguments = invocation.getArguments();
             Object[] argumentsWithoutFirst = Arrays.copyOfRange(allArguments, 1, allArguments.length);
-            inputStream.printf((String) allArguments[0], argumentsWithoutFirst);
+            actualOutputStrings.add(String.format((String) allArguments[0], argumentsWithoutFirst));
             return null;
         }).when(ioService).printFormattedLine(anyString(), any(Object[].class));
 
         when(questionDao.findAll()).thenReturn(expectedQuestions);
+
         testService.executeTest();
 
-        String actualOutputText = outputStreamCaptor.toString();
-        List<String> actualOutputQuestions = exceptedOutputQuestions.stream()
-                .filter(actualOutputText::contains)
-                .toList();
+        actualOutputStrings.retainAll(exceptedOutputQuestions);
 
-        assertThat(actualOutputQuestions).isEqualTo(exceptedOutputQuestions);
+        assertThat(actualOutputStrings).isEqualTo(exceptedOutputQuestions);
     }
 }
-
-/*
-Please answer the questions below
-
-Is there life on Mars?
-1) Science doesn't know this yet; 2) Certainly. The red UFO is from Mars. And green is from Venus; 3) Absolutely not;
-
-How should resources be loaded form jar in Java?
-1) ClassLoader#geResourceAsStream or ClassPathResource#getInputStream; 2) ClassLoader#geResource#getFile + FileReader; 3) Wingardium Leviosa;
-
-Which option is a good way to handle the exception?
-1) @SneakyThrow; 2) e.printStackTrace(); 3) Rethrow with wrapping in business exception (for example, QuestionReadException); 4) Ignoring exception;
-
-What is the meaning of life?
-
-What number did I make a wish for?
-1) 0; 2) 1; 3) 2; 4) 3; 5) 4; 6) 5; 7) 6; 8) 7; 9) 8; 10) 9;
- */
