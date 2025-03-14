129dc83c700f5debc1a6eff5ddd321f09811297a
maks <maks-xrenov@mail.ru>
2025-03-14 13:19:30 +0300

    [HW-04] Скопировал ДЗ 3

--- hw04/src/main/java/ru/otus/hw/service/TestRunnerServiceImpl.java
+++ hw04/src/main/java/ru/otus/hw/service/TestRunnerServiceImpl.java
@@ -2,6 +2,7 @@ package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.otus.hw.exceptions.QuestionReadException;
 
 @Service
 @RequiredArgsConstructor
@@ -13,10 +14,16 @@ public class TestRunnerServiceImpl implements TestRunnerService {
 
     private final ResultService resultService;
 
+    private final LocalizedIOService ioService;
+
     @Override
     public void run() {
-        var student = studentService.determineCurrentStudent();
-        var testResult = testService.executeTestFor(student);
-        resultService.showResult(testResult);
+        try {
+            var student = studentService.determineCurrentStudent();
+            var testResult = testService.executeTestFor(student);
+            resultService.showResult(testResult);
+        } catch (QuestionReadException e) {
+            ioService.printLineLocalized("TestRunnerImpl.error.reading.questions");
+        }
     }
 }
