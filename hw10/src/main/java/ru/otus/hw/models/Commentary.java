1c0b4f2fb153293bcd3b5d6f85c3a4b1c0064152
Maksim <maks-xrenov@mail.ru>
2025-05-21 08:55:29 +0300

    [HW-10] Скопировал прошлое ДЗ

---
+++ hw10/src/main/java/ru/otus/hw/models/Commentary.java
@@ -0,0 +1,33 @@
+package ru.otus.hw.models;
+
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "commentaries")
+@Entity
+public class Commentary {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+    @Column(name = "text", nullable = false)
+    private String text;
+}
